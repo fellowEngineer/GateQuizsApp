7888fb68f02209212af9aff47c8b9344bc2e031a
fellowEngineer <bsuman1921@gmail.com>
2024-07-26 14:31:03 +0530

    Adding some more page to construct the structure

--- App.js
+++ App.js
@@ -7,8 +7,33 @@ App.use(express.static("public"));
 
 
 App.get('/', (req, res) => {
-    res.render("gq")
+    res.render("gqa")
 })
+
+App.get('/aptitude', (req, res) => {
+    res.render("p/aptitude-gqa")
+})
+
+App.get('/random', (req, res) => {
+    res.render("p/random-gqa")
+})
+
+App.get('/subject', (req, res) => {
+    res.render("p/subject-gqa")
+})
+
+App.get('/stream', (req, res) => {
+    res.render("p/stream-gqa")
+})
+
+App.get('/login', (req, res) => {
+    res.render("p/login-gqa")
+})
+
+App.get('/signup', (req, res) => {
+    res.render("p/signup-gqa")
+})
+
 
 const PORT = process.env.PORT || 5678;
 
